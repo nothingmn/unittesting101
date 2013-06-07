b1bb4d20cfe6267ee150676ab7dee52c85208517
Rob Chartier <rob.chartier@gmail.com>
2013-06-07 15:57:16 -0700

    initial import - level 1 and 2 drafts done

---
+++ Source/Bank/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        // class under test
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount will result in a negative balance";
+
+        public BankAccount(string name, double startingBalance)
+        {
+            Name = name;
+            Balance = startingBalance;
+
+        }
+
+        public string Name { get; set; }
+        public double Balance { get; set; }
+
+        private readonly object _balanceLock = new object();
+        public void Debit(double amount)
+        {
+            lock (_balanceLock)
+            {
+                double newBalance = Balance - Math.Abs(amount);
+                if (amount > Balance)
+                {
+                    throw new ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
+                }
+                if (newBalance < 0)
+                {
+                    throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
+                }
+                System.Threading.Thread.Sleep(100);
+                Balance = newBalance;
+            }
+        }
+
+        public void Credit(double amount)
+        {
+            lock (_balanceLock)
+            {
+                System.Threading.Thread.Sleep(10);
+                Balance += Math.Abs(amount);
+            }
+        }
+
+    }
+}
---
+++ Source/BankTest/BankAccountTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank;
+using NUnit.Framework;
+
+namespace BankTest
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+        [Test]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+
+        [Test]
+        [ExpectedException(typeof (ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert is handled by ExpectedException
+        }
+
+        [Test]
+        //[ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsGreaterThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert
+                StringAssert.StartsWith(BankAccount.DebitAmountExceedsBalanceMessage, e.Message);
+                return;
+            }
+            Assert.Fail("No exception was thrown");
+        }
+        [Test]
+        public void Credit_Simple_Amount_UpdatesBalance()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double creditAmount = 4.55;
+            double expected = 16.53;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Credit(creditAmount);
+
+            // assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.01, "Account not credited correctly");
+        }
+
+        [Test]
+        public void Debit_Multi_Threaded()
+        {
+            // arrange
+            double beginningBalance = 10;
+            double debitAmount = -1;
+            double expected = 0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Run(() => account.Debit(debitAmount)));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            // assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.01, "Account not debited correctly");
+            
+        }
+
+        [Test]
+        public void Debit_And_Credit_Multi_Threaded()
+        {
+            // arrange
+            double beginningBalance = 10;
+            double debitAmount = -1;
+            double creditAmount = 1;
+            double expected = 10;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Run(() => account.Debit(debitAmount)));
+                tasks.Add(Task.Run(() => account.Credit(creditAmount)));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            // assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.01, "Account balance is not correct");
+
+        }
+    }
+}
